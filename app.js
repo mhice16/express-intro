2beaa27141dd08086aada5c22954aea713341c4a
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2016-12-21 11:39:29 -0700

    Implemented GET all products

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ app.get('/', function(req, res){
   res.send("<h1>Hello World!</h1>");
 })
 
+app.get('/api/products', (req, res) => {
+  let products = [
+    {name: "JavaScript Book", price: 9.99},
+    {name: "A Christmas Carol", price: 19.99}
+  ];
+  let stringified = JSON.stringify(products);
+  res.send(stringified);
+})
+
 app.listen(3000, function() {
   console.log("Listening on port 3000.  Go to http://localhost:3000/");
 })
